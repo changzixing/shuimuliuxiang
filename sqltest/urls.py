3a11af3a9700dafdbb0b7a8ce8a0304277761ee8
MarcoSSJ <sunsj16@mails.tsinghua.edu.cn>
2019-11-17 12:45:40 +0800

    sqltest

---
+++ sqltest/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # path('logon', views.logon_req, name='logon'),
+    # path('record/add', views.add_req, name='add'),
+    # path('record/<record_id>/delete', views.delete_req, name='delete'),
+    # path('record/<record_id>', views.get_id, name='get'),
+]
