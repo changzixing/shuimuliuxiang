f05d3e1c60509999df48eb1a1eaa8b47acb16ec1
MarcoSSJ <sunsj16@mails.tsinghua.edu.cn>
2020-01-04 17:41:37 +0800

    ci test

--- shuimu_backends/wechatAPP/tests.py
+++ shuimu_backends/wechatAPP/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
-
 # Create your tests here.
+from . import views
+import requests
+# import pytest
+import json
+
+class TestClass(TestCase):
+
+    def test_get_my_activity_0(self):
+        url = "http://localhost:8000/get_my_activity"
+        data = {"openID": "wrongopenid", "activityStatus": "willdo", "pageNum": '1'}
+        res = requests.post(url=url, data=data).json()
+        assert res['error'] == 'no such user'
+
+    def test_get_my_activity_1(self):
+        url = "http://localhost:8000/get_my_activity"
+        data = {"openID": "oo85p5LEN2BJ8rf1WJE0m03iM0lY", "activityStatus": "willdo", "pageNum": '1'}
+        res = requests.post(url=url, data=data).json()
+        assert res['content'] == [] or res['content']
+
+    def test_get_my_activity_2(self):
+        url = "http://localhost:8000/get_my_activity"
+        data = {"openID": "oo85p5LEN2BJ8rf1WJE0m03iM0lY", "activityStatus": "doing", "pageNum": '1'}
+        res = requests.post(url=url, data=data).json()
+        assert res['content'] == [] or res['content']
+
+    def test_get_my_activity_3(self):
+        url = "http://localhost:8000/get_my_activity"
+        data = {"openID": "oo85p5LEN2BJ8rf1WJE0m03iM0lY", "activityStatus": "done", "pageNum": '1'}
+        res = requests.post(url=url, data=data).json()
+        assert res['content'] == [] or res['content']
+
+    def test_edit_user_0(self):
+        url = "http://localhost:8000/edit_user"
+        data = {"openID": "wrongid"}
+        res = requests.post(url=url, data=data).json()
+        assert res['error'] == 'no such user'
+
+    def test_edit_user_1(self):
+        url = "http://localhost:8000/edit_user"
+        data = {"openID": "oo85p5LEN2BJ8rf1WJE0m03iM0lY", "sex": "male"}
+        res = requests.post(url=url, data=data).json()
+        assert res['result'] == 'edit succeeded'
+
+    def test_send_activity_info_0(self):
+        url = "http://localhost:8000/send_activity_info"
+        data = {"openID": "wrongid"}
+        res = requests.post(url=url, data=data).json()
+        assert res['error']
+
+    def test_send_activity_info_1(self):
+        url = "http://localhost:8000/send_activity_info"
+        data = {"openID": "oo85p5LEN2BJ8rf1WJE0m03iM0lY", "activityNum": "wrongnum"}
+        res = requests.post(url=url, data=data).json()
+        assert res['error']
+
+    def test_send_activity_info_2(self):
+        url = "http://localhost:8000/send_activity_info"
+        data = {"openID": "oo85p5LEN2BJ8rf1WJE0m03iM0lY", "activityNum": "10001"}
+        res = requests.post(url=url, data=data).json()
+        assert res['activityName']
